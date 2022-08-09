dd4070efa624342c82934a56d083fb283cdc4fe0
dajung <ekwjd990913@gmail.com>
2022-08-09 09:14:47 +0900

    Stream을 이용한 구구단 계산

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,12 +1,12 @@
 package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
+    private int increaseNo = -1;
+
     @RequestMapping("/sbb")
     // 아래 함수의 리턴값을 그대로 브라우저에 표시
     // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답의 바디에 담는다.
@@ -16,5 +16,53 @@ public class MainController {
         System.out.println("Hello");
         // 먼 미래에 브라우저에서 보여짐
         return "안녕하세요.";
+    }
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showPage1() {
+        return """
+                <form method="POST" action="/page2">
+                    <input type="number" name="age" placeholder="나이" />
+                    <input type="submit" value="page2로 POST 방식으로 이동" />
+                </form>
+                """;
+    }
+
+    @PostMapping("/page2")
+    @ResponseBody
+    public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
+                """.formatted(age);
+    }
+
+    @GetMapping("/page2")
+    @ResponseBody
+    public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
+                """.formatted(age);
+    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int showPlus(int a, int b) {
+        return a + b;
+    }
+
+    @GetMapping("/minus")
+    @ResponseBody
+    public int showMinus(int a, int b) {
+        return a - b;
+    }
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public int showIncrease() {
+        increaseNo++;
+
+        return increaseNo;
     }
 }
