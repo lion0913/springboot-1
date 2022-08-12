8c82e4b789708970e3c031eae1c7ceefdfc4dabc
dajung <ekwjd990913@gmail.com>
2022-08-12 13:31:13 +0900

    게시판 조회 테스트

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
@@ -34,7 +34,28 @@ public class AnswerRepositoryTest {
 
     private void createSampleData() {
         QuestionRepositoryTest.createSampleData(questionRepository);
+
+        Question q = questionRepository.findById(1).get();
+
+        Answer a1 = new Answer();
+        a1.setContent("sbb는 질문답변 게시판 입니다.");
+        a1.setQuestion(q);
+        a1.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a1);
+
+        Answer a2 = new Answer();
+        a2.setContent("sbb에서는 주로 스프링부트관련 내용을 다룹니다.");
+        a2.setQuestion(q);
+        a2.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a2);
     }
+
+    @Test
+    void 조회() {
+        Answer a = this.answerRepository.findById(1).get();
+        assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판 입니다.");
+    }
+
 
     @Test
     void 저장() {
