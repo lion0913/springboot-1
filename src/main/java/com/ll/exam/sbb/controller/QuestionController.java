83b642887e13987ba8dabedadbcec5f1064e0e59
dajung <ekwjd990913@gmail.com>
2022-08-16 10:18:50 +0900

    question_list 템플릿 추가

--- src/main/java/com/ll/exam/sbb/controller/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/controller/QuestionController.java
@@ -1,22 +1,40 @@
 package com.ll.exam.sbb.controller;
 
 
+import com.ll.exam.sbb.entity.Question;
+import com.ll.exam.sbb.repository.QuestionRepository;
 import com.ll.exam.sbb.service.QuestionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class QuestionController
 {
-    @Autowired
-    private QuestionService questionService;
+
+    private final QuestionRepository questionRepository;
 
     @RequestMapping("/question/list")
-    @ResponseBody
-    public String showList() {
+    public String showList(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+
+        // 미래에 실행된 question_list.html 에서
+        // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
+        model.addAttribute("questionList", questionList);
+
         return "question_list";
+    }
+
+    @RequestMapping("/question/list2")
+    @ResponseBody
+    public String list2() {
+        return "하하111777";
     }
 }
