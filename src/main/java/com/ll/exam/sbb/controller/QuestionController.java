13faeb5d01b0cebd4dbd91985d52493fed9d1bfd
dajung <ekwjd990913@gmail.com>
2022-08-16 13:15:30 +0900

    디테일 화면 구현

--- src/main/java/com/ll/exam/sbb/controller/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/controller/QuestionController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,12 +19,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class QuestionController
 {
-
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @RequestMapping("/question/list")
     public String showList(Model model) {
-        List<Question> questionList = questionRepository.findAll();
+        List<Question> questionList = questionService.findAll();
 
         // 미래에 실행된 question_list.html 에서
         // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
@@ -32,9 +32,10 @@ public class QuestionController
         return "question_list";
     }
 
-    @RequestMapping("/question/list2")
-    @ResponseBody
-    public String list2() {
-        return "하하111777";
+    @RequestMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable int id) {
+        Question question = questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/ll/exam/sbb/service/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/service/QuestionService.java
@@ -2,12 +2,21 @@ package com.ll.exam.sbb.service;
 
 import com.ll.exam.sbb.entity.Question;
 import com.ll.exam.sbb.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor //final 붙은 것에 대한 생성자가 만들어짐
 public class QuestionService {
-    @Autowired
-    private QuestionRepository questionRepository;
+    private final QuestionRepository questionRepository;
+
+//    public QuestionService(QuestionRepository questionRepository) {
+//        this.questionRepository = questionRepository;
+//    }
 
     public Question findById(int id) {
         Question q1 = questionRepository.findById(2).get();
@@ -15,5 +24,15 @@ public class QuestionService {
         System.out.println(q2.getAnswerList());
 
         return q2;
+    }
+
+    public List<Question> findAll() {
+        return questionRepository.findAll();
+    }
+
+    public Question getQuestion(int id) {
+        Question q = questionRepository.findById(id).orElse(null);
+
+        return q;
     }
 }
