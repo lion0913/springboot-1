f3dca8e56d3d3f01edefb2b82e38365db0951db1
dajung <ekwjd990913@gmail.com>
2022-08-17 13:15:51 +0900

    질문 등록 처리 완료

--- src/main/java/com/ll/exam/sbb/controller/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/controller/QuestionController.java
@@ -9,19 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor
 public class QuestionController
 {
     private final QuestionService questionService;
 
-    @RequestMapping("/question/list")
+    @GetMapping("/list")
     public String showList(Model model) {
         List<Question> questionList = questionService.findAll();
 
@@ -32,10 +31,21 @@ public class QuestionController
         return "question_list";
     }
 
-    @RequestMapping("/question/detail/{id}")
+    @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable int id) {
         Question question = questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String create(@RequestParam String subject, String content) {
+        questionService.create(subject, content);
+        return "redirect:/question/list";
     }
 }
--- src/main/java/com/ll/exam/sbb/service/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/service/QuestionService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,5 +23,14 @@ public class QuestionService {
         Question q = questionRepository.findById(id).orElse(null);
 
         return q;
+    }
+
+    public void create(String subject, String content) {
+        Question question = new Question();
+        question.setCreatedDate(LocalDateTime.now());
+        question.setSubject(subject);
+        question.setContent(content);
+
+        questionRepository.save(question);
     }
 }
