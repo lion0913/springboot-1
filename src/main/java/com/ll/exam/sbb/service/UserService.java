50ca0203f93f896104643643e351f0a33540b9f4
dajung <ekwjd990913@gmail.com>
2022-08-18 13:53:10 +0900

    passwordencoder 객체의 생성을 스프링이 관리하도록

--- src/main/java/com/ll/exam/sbb/service/UserService.java
+++ src/main/java/com/ll/exam/sbb/service/UserService.java
@@ -5,6 +5,7 @@ import com.ll.exam.sbb.repository.SiteUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.realm.UserDatabaseRealm;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -12,12 +13,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final SiteUserRepository siteUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String email, String password) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
         user.setPassword(passwordEncoder.encode(password));
 
         siteUserRepository.save(user);
